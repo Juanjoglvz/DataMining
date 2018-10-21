3a49c869940bdc8de7e8702f02bef6e4bf25d266
JUAN JOSE CORROTO MARTIN <JuanJose.Corroto@alu.uclm.es>
2018-10-21 18:26:43 +0200

    cookiecutter Directory structure added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Projecto for the Data Mining subject',
+    author='Javier Cordoba, Juan Jose Corroto, Alvaro Guerrero, Fernando Vallejo',
+    license='',
+)
