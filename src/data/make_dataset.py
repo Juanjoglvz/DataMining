f9f6121e026f8a8532756ce23afcbc09e1352fb8
JUAN JOSE CORROTO MARTIN <JuanJose.Corroto@alu.uclm.es>
2018-10-21 20:32:32 +0200

    Reading data

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,30 +1,29 @@
-# -*- coding: utf-8 -*-
-import click
-import logging
-from pathlib import Path
-from dotenv import find_dotenv, load_dotenv
+import pandas as pd
 
 
-@click.command()
-@click.argument('input_filepath', type=click.Path(exists=True))
-@click.argument('output_filepath', type=click.Path())
-def main(input_filepath, output_filepath):
-    """ Runs data processing scripts to turn raw data from (../raw) into
-        cleaned data ready to be analyzed (saved in ../processed).
-    """
-    logger = logging.getLogger(__name__)
-    logger.info('making final data set from raw data')
+
+# We read the csv
+df118 = pd.read_csv("../../data/raw/0118.csv")
+df218 = pd.read_csv("../../data/raw/0218.csv")
+df318 = pd.read_csv("../../data/raw/0318.csv")
+df418 = pd.read_csv("../../data/raw/0418.csv")
+df417 = pd.read_csv("../../data/raw/0417.csv")
+df518 = pd.read_csv("../../data/raw/0518.csv")
+df517 = pd.read_csv("../../data/raw/0517.csv")
+df617 = pd.read_csv("../../data/raw/0617.csv")
+df717 = pd.read_csv("../../data/raw/0717.csv")
+df817 = pd.read_csv("../../data/raw/0817.csv")
+df917 = pd.read_csv("../../data/raw/0917.csv")
+df1017 = pd.read_csv("../../data/raw/1017.csv")
+df1117 = pd.read_csv("../../data/raw/1117.csv")
+df1217 = pd.read_csv("../../data/raw/1217.csv")
 
 
-if __name__ == '__main__':
-    log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-    logging.basicConfig(level=logging.INFO, format=log_fmt)
+# We new Create a new dataframe adding everything
+df = df118.append([df218, df318, df418, df518, df417, df517, df617, df717, df817, df917, df1017, df1117, df1217])
 
-    # not used in this stub but often useful for finding various files
-    project_dir = Path(__file__).resolve().parents[2]
+print(df.columns)
 
-    # find .env automagically by walking up directories until it's found, then
-    # load up the .env entries as environment variables
-    load_dotenv(find_dotenv())
+df = df.drop('Unnamed: 29', axis = 1)
 
-    main()
+df.to_csv('../../data/interim/vuelos.csv', index=False)
